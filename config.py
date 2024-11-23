97673c13eaf672530157b77c18444d35ff91b1cb
MrKali7 <sandeepjangra1011@gmail.com>
2024-11-23 21:24:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6748708552:AAEya6awhAuxwhR7ujfPWsko7j1GkCjPWjk")
-APP_ID = int(os.environ.get("APP_ID", "21863401"))
-API_HASH = os.environ.get("API_HASH", "5f7d3dc656c008c70bffdf7f4f9b8ebb")
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001830594381"))
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+APP_ID = int(os.environ.get("APP_ID", "24984353"))
+API_HASH = os.environ.get("API_HASH", "d188b95fd99b5a5410b8eef5fac7f132")
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002427274779"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "6230356497"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6147843565"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
@@ -22,18 +22,18 @@ SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "offerlinks.in")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "2b491547a49b1d8274e9a619c8258029ef2b8bd0")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/How_to_Download_7x/35") 
+TUT_VID = os.environ.get("TUT_VID", "@All_links_ki_Duniya") 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002091491280"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}, Thanks for using me :D @team_netflix ⚡️.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}, 📂 Welcome to FileStore Bot!👋 Hi there! I'm here to help you store, manage, and retrieve your files effortlessly.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6497757690").split()):
+    for x in (os.environ.get("ADMINS", "6147843565").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -47,7 +47,7 @@ AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "This file will be automatic
 AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "Your file has been successfully deleted. Thank you for using our service. ✅")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @codeflix_bots</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>•Caption</b>")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
@@ -56,10 +56,10 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
+USER_REPLY_TEXT = ""Woo! You're not my Owner!""
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6497757690)
+ADMINS.append(6147843565)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
